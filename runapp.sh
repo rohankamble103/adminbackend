6cc0ca0c7c56512558fb49c7357d2b1fc258b77e
Rohan Kamble <124754978+rohankamble103@users.noreply.github.com>
2024-01-17 12:12:12 +0530

    Update runapp.sh
    sudo

--- runapp.sh
+++ runapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install -g pm2
-npm install
-npm install nodemon
-pm2 start dist/app.js
+sudo npm install -g pm2
+sudo npm install
+sudo npm install nodemon
+sudo pm2 start dist/app.js
